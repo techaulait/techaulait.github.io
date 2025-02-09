c2d74fc9d7c1d3df28dbab40ee00f584ec5ba27f
shun7538 <matsumodel@gmail.com>
2025-02-09 21:23:53 +0900

    homepageの修正

--- homepage/vite.config.ts
+++ homepage/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 相対パスに変更
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
